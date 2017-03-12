63185701bb092992608d17b149bd0f917279d17e
shashank2789 <shashank.joshi30@gmail.com>
2017-03-12 04:24:41 +0000

    s

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ routes(app);
 
 api(app);
 
-app.listen(port,function(){
-  
-  console.log("working h ji");
-})
+pp.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
